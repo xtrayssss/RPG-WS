bb92de8c2ab46a51bc6994dc86921601704ec504
xtrayssss <pkneskill123@gmail.com>
2023-02-01 20:07:45 +0500

    FlipPlayer

--- qwerty/Assets/Character/Scripts/AnimationController/AnimationManager.cs
+++ qwerty/Assets/Character/Scripts/AnimationController/AnimationManager.cs
@@ -2,16 +2,46 @@
 
 namespace Assets.Character.Scripts.AnimationManager
 {
+    [RequireComponent(typeof(PlayerInputHandler), typeof(Animator))]
     class AnimationManager : MonoBehaviour
     {
         [SerializeField] Animator animator;
 
         [SerializeField] private PlayerMove player;
 
+        [SerializeField] private float totalTimerIdleState;
+        
+        private float timerIdleState;
+
+        private const bool isMove = false;
+
+        private const string animBoolName = "isMove";
+
+        private void Awake()
+        {
+            timerIdleState = totalTimerIdleState;
+        }
         private void Update()
         {
-            animator.SetBool("isMove", !player.hasMoved);
+            AnimationMovePlayer();
         }
 
+        private void AnimationMovePlayer()
+        {
+            if (!player.hasMoved || player.player.moveInput == Vector2.zero)
+            {
+                timerIdleState -= Time.deltaTime;
+            }
+            if (timerIdleState <= 0 && player.player.moveInput == Vector2.zero)
+            {
+                animator.SetBool(animBoolName, isMove);
+
+                timerIdleState = totalTimerIdleState;
+            }
+            if (!player.hasMoved)
+            {
+                animator.SetBool(animBoolName, !isMove);
+            }
+        }
     }
 }
---
+++ qwerty/Assets/Character/Scripts/Flip/FlipPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Assets.Character.Scripts
+{
+    [RequireComponent(typeof(PlayerInputHandler), typeof(PlayerMove))]
+    public class FlipPlayer : MonoBehaviour
+    {
+        [SerializeField] PlayerInputHandler inputHandler;
+        [SerializeField] PlayerMove playerMove;
+
+        private void Update()
+        {
+            Flip();
+        }
+        private void Flip()
+        {
+            if (inputHandler.moveInput.x == -1 && playerMove.hasMoved)
+            {
+                transform.localScale = new Vector3(-0.0442966f, 0.0442966f, 0.0442966f);
+            }
+            if (inputHandler.moveInput.x == 1 && playerMove.hasMoved)
+            {
+                transform.localScale = new Vector3(0.0442966f, 0.0442966f, 0.0442966f);
+            }
+        }
+    }
+}
